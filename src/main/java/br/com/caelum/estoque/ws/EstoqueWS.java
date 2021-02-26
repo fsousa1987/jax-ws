e20b7f4fc792ef82bedf52754ae2c454f7114902
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-26 05:55:24 -0300

    WSDL abstrato e concreto

--- src/main/java/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/main/java/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -11,7 +11,7 @@ import jakarta.jws.WebService;
 
 import java.util.List;
 
-@SuppressWarnings({"FieldMayBeFinal", "CanBeFinal"})
+@SuppressWarnings({"FieldMayBeFinal", "CanBeFinal", "NonJaxWsWebServices"})
 @WebService
 public class EstoqueWS {
 	
