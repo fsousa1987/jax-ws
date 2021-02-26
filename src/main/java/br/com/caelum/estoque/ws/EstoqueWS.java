4e536473099e2b1fdb27f7d0e553f7effa492b33
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-26 10:08:24 -0300

    Entendendo os estilos Document e RPC

--- src/main/java/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/main/java/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -8,11 +8,15 @@ import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebResult;
 import jakarta.jws.WebService;
+import jakarta.jws.soap.SOAPBinding;
 
 import java.util.List;
 
+import static jakarta.jws.soap.SOAPBinding.*;
+
 @SuppressWarnings({"FieldMayBeFinal", "CanBeFinal", "NonJaxWsWebServices"})
 @WebService
+@SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
@@ -28,12 +32,29 @@ public class EstoqueWS {
 		return new ListaItens(lista);
 	}
 
-	@WebMethod(operationName = "CadastrarItem")
+	@WebMethod(action = "CadastrarItem", operationName = "CadastrarItem")
 	@WebResult(name = "item")
 	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
 							  @WebParam(name="item")Item item) throws AutorizacaoException {
 
 		System.out.println("cadastrando item " + item + ", Token: " + token);
+
+		boolean valido = new TokenDao().ehValido(token);
+
+		if (!valido) {
+			throw new AutorizacaoException("Autorização falhou");
+		}
+
+		new ItemValidador(item).validate();
+
+		this.dao.cadastrar(item);
+		return item;
+	}
+
+	@WebMethod(operationName = "CadastrarItem2")
+	@WebResult(name = "item")
+	public Item cadastrarItem2(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
+							  @WebParam(name="item")Item item) throws AutorizacaoException {
 
 		boolean valido = new TokenDao().ehValido(token);
 
